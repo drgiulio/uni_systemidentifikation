0c7db24046b7f96e2467d255983f93a70a05eeeb
Giulio <giulio@Giulios-MBP.fritz.box>
2018-11-28 09:33:16 +0100

    System paramter definition improvements & additonal plots

--- init.m
+++ init.m
@@ -13,10 +13,12 @@ x1_init = 1;
 x2_init = 0;
 
 % Duffing system
-gamma = 2.1; % amplitude of forced oscillation
-omega = 1.8; % angular frequency
-delta = 0.4; % damping
-alpha = -1.1; % linear term
+gamma = 0; % amplitude of forced oscillation
+f = 1; % Eigenfrequency
+omega = 2*pi*f;%1.8; % angular frequency
+q = 50; % Quality factor of oscillation (inversely proportional to damping)
+delta = omega/q; % damping
+alpha = omega^2; % linear term
 beta = 1; % Duffing term
 
 % Noise
@@ -40,3 +42,25 @@ h = ylabel('$x_2$');
 set(h,'Interpreter','LaTex','FontSize',12)
 h = title('Phase portrait');
 set(h,'Interpreter','LaTex','FontSize',12)
+
+% Time plot states
+figure
+subplot(2,1,1)
+plot(t, x1)
+grid
+h = ylabel('$x_1$');
+set(h,'Interpreter','LaTex','FontSize',12)
+h = xlabel('$t$');
+set(h,'Interpreter','LaTex','FontSize',12)
+h = title('Time plots');
+set(h,'Interpreter','LaTex','FontSize',12)
+subplot(2,1,2)
+plot(t, x2)
+grid
+h = ylabel('$x_2$');
+set(h,'Interpreter','LaTex','FontSize',12)
+h = xlabel('$t$');
+set(h,'Interpreter','LaTex','FontSize',12)
+
+% TO DO: Plot frequency response
+
