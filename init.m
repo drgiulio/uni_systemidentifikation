17df7597613f5999b306f8653a91a31405d1f618
Giulio <giulio@Giulios-MBP.fritz.box>
2018-12-01 11:31:38 +0100

    Duffing system new shit: frequency response and parameter settings for different scenarios

--- init.m
+++ init.m
@@ -6,19 +6,17 @@ close all
 
 % Simulation settings
 hsim = 0.001;
-tsim = 50;
+tsim = 200;
 
 % Initial states
 x1_init = 1;
 x2_init = 0;
 
 % Duffing system
-gamma = 1; % amplitude of forced oscillation
-f = 1; % Eigenfrequency
-omega = 2*pi*f;%1.8; % angular frequency
-q = 10; % Quality factor of oscillation (inversely proportional to damping)
-delta = omega/q; % damping
-alpha = omega^2; % linear term
+gamma = 0.2;%0.28;%0.29;%0.37;%0.65;%1;%0.5; % amplitude of forced oscillation
+omega = 1.2; % angular frequency
+delta = 0.3; % damping
+alpha = -1; % linear term
 beta = 1; % Duffing term
 
 % Noise
@@ -28,67 +26,51 @@ var = 0.00001;
 %% Run
 [t, y] = sim('duffing_plant');
 
-%% Plot
+%% Plots
 x1 = y(:,1);
 x2 = y(:,2);
 
 % Phase plot
 figure
+subplot(3,1,1)
 plot(x1, x2)
 grid
 h = xlabel('$x_1$');
 set(h,'Interpreter','LaTex','FontSize',12)
 h = ylabel('$x_2$');
 set(h,'Interpreter','LaTex','FontSize',12)
-h = title('Phase portrait');
+h = title(['Phase portrait: $\gamma=', num2str(gamma),'$, $\alpha=', num2str(alpha),'$, $\beta=', num2str(beta),'$, $\delta=', num2str(delta),'$, $\omega=', num2str(omega),'$']);
 set(h,'Interpreter','LaTex','FontSize',12)
 
 % Time plot states
-figure
-subplot(2,1,1)
+subplot(3,1,2)
 plot(t, x1)
 grid
 h = ylabel('$x_1$');
 set(h,'Interpreter','LaTex','FontSize',12)
 h = xlabel('$t$');
 set(h,'Interpreter','LaTex','FontSize',12)
-h = title('Time plots');
+h = title('Time plot');
 set(h,'Interpreter','LaTex','FontSize',12)
-subplot(2,1,2)
+%{
+subplot(4,1,3)
 plot(t, x2)
 grid
 h = ylabel('$x_2$');
 set(h,'Interpreter','LaTex','FontSize',12)
 h = xlabel('$t$');
 set(h,'Interpreter','LaTex','FontSize',12)
+%}
 
-% Compute frequency response via Fixpunktiteration
-freq = linspace(0,1.5*f,10e4);%2*f,10e4);
-z = zeros(size(freq));
-NofIter = 10;
-z_start = 0.9*gamma;
-
-for i = 1:length(freq)
-    om = 2*pi*freq(i);
-    z_current = z_start;
-    z_next = 0;
-    for n = 1:NofIter
-        z_next = (gamma^2 / z_current) / ( (om^2 - alpha - 0.75*beta*z_current^2)^2 + (delta*om)^2 );
-        z_current = z_next;
-    end
-    z(i) = z_current;
-end
-
-% Plot
-figure
-plot(2*pi*freq, z/gamma)
+% Compute frequency response and plot
+subplot(3,1,3)
+% normlaized amplitude y = z/gamma
+func = @(omega,y) ( ((omega.^2 - alpha)/gamma - 0.75*beta*y.^2).^2 + (delta/gamma*omega).^2 ) .* y.^2 - 1/(gamma^2);
+fimplicit(func,'XRange', [0 2*omega], 'YRange', [0 4])%,'MeshDensity',152)
 grid
-h = xlabel('$\omega=2\pi f$');
+h = xlabel('$\omega$');
 set(h,'Interpreter','LaTex','FontSize',12)
 h = ylabel('$z/\gamma$');
 set(h,'Interpreter','LaTex','FontSize',12)
-h = title('Phase portrait');
+h = title('Frequency response');
 set(h,'Interpreter','LaTex','FontSize',12)
-
-
-
